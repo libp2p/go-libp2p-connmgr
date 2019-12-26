6b16e336bd10af1654a36fdc60143d7357900abf
Raúl Kripalani <raul@protocol.ai>
2019-12-26 17:21:24 +0000

    document options.

--- options.go
+++ options.go
@@ -1,9 +1,9 @@
 package connmgr
 
-import (
-	"time"
-)
+import "time"
 
+// BasicConnManagerConfig is the configuration struct for the basic connection
+// manager.
 type BasicConnManagerConfig struct {
 	highWater     int
 	lowWater      int
@@ -12,8 +12,10 @@ type BasicConnManagerConfig struct {
 	decayer       *DecayerCfg
 }
 
+// Option represents an option for the basic connection manager.
 type Option func(*BasicConnManagerConfig) error
 
+// DecayerConfig applies a configuration for the decayer.
 func DecayerConfig(opts *DecayerCfg) Option {
 	return func(cfg *BasicConnManagerConfig) error {
 		cfg.decayer = opts
