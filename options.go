a7820a5879c799f55755928575fe6106b5b216b5
Marten Seemann <martenseemann@gmail.com>
2022-01-15 18:58:17 +0400

    hide watchdog behind cgo flag

--- options.go
+++ options.go
@@ -51,11 +51,3 @@ func WithSilencePeriod(p time.Duration) Option {
 		return nil
 	}
 }
-
-// WithEmergencyTrim is an option to enable trimming connections on memory emergency.
-func WithEmergencyTrim(enable bool) Option {
-	return func(cfg *config) error {
-		cfg.emergencyTrim = enable
-		return nil
-	}
-}
